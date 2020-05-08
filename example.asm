91ae3cbf9c8b1029a93a296e9baca4584ad99f04
danilpiat <56876348+danilpiat@users.noreply.github.com>
2020-05-08 19:02:18 +0600

    Add files via upload

--- example.asm
+++ example.asm
@@ -1,31 +1,65 @@
-.model   small
-.stack   100h
-.data 
-string      dw   1010111010111010b
-count      db   0
+.model small
+.stack 100h
+.486
+.data
+m db 4
+n db 5
+matrix  db -1, 2, 3, 4, 5
+	db 6, 7, 8, 9, 10
+	db 11, 12, -13, 14, 15
+	db -16, 17, 18, 19, -20
+vector  dw 0,0,0,0
+
 .code
 
-begin:     
-mov ax, @data
-mov ds, ax
+	mov ax, @data
+	mov ds, ax
 
-      mov   cx,string
-      mov   bx,010b
-      mov   dx,111b
-      
-p1:      
-      push   cx
-      and   cx,dx
-      xor   cx,bx
-      jnz   p2
-      inc    count
-p2:      
-      pop   cx
-      shl   bx,1
-      shl   dx,1
-      jnc   p1
 
-	 mov ax,4c00h
-	 int 21h
+	mov si,offset matrix
+   	mov di,offset vector
+	xor dx,dx
+	mov dl,m
 
-end      
+cicle0:
+
+	xor cx,cx
+	mov cl,n
+	xor ax,ax
+	xor bx,bx
+cicle1:
+	lodsb
+	cbw
+	add bx,ax
+	loop cicle1
+	mov [di],bx
+	add di,2	
+	dec dx
+	test dx,dx
+	jnz cicle0
+
+sort:
+	mov dx,0
+	xor cx,cx
+	mov cl,m
+	dec cx
+	mov si,offset vector
+cicle2:
+        lodsw 
+        mov bx,[si]
+        cmp bx,ax
+        jg skipswap
+        mov [si],ax       
+	mov [si-2],bx 
+	inc dx
+skipswap:
+        
+        loop cicle2
+        test dx,dx
+	jnz sort
+
+	
+	mov ax,4C00h
+	int 21h	
+ end 	
+
