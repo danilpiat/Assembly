0194b739c857c9a28c579c458839b2b3b610e633
danilpiat <56876348+danilpiat@users.noreply.github.com>
2020-06-05 19:19:00 +0600

    Add files via upload

--- example.asm
+++ example.asm
@@ -1,31 +1,115 @@
-.model   small
-.stack   100h
-.data 
-string      dw   1010111010111010b
-count      db   0
-.code
 
-begin:     
-mov ax, @data
-mov ds, ax
+	.model small
+	.stack 100h
+	.486
+	.data
+string db '  ; all those  ,,	moments   will be lost in time,like  tears  ; in rain; ', 0
+delim db ' ,;', 0
+newstring db 100 dup(?) ; for result
+	
+	.code
+	mov ax, @data
+	
+	mov ds, ax
+	mov es, ax
+	cld
+	lea si, string 
+	lea di, newstring 
+	
+m1:	call space 
+	cmp byte ptr [si], 0
+	je fin
+	call _word
+	
+	push ax
+	call reverse
+	inc sp
+	inc sp
+	cmp byte ptr [si], 0
+	je fin
+	mov al, ' '
+	stosb
+	jmp m1
+	
+fin:
+	xor al, al
+	stosb
+	mov ax, 4c00h
+	int 21h
+; code: 
+; 
+reverse proc c uses ax bx cx;
+	locals @@
+	push bp
+	mov bp, sp
+	mov cx, [bp+10]
+	sub cx, si
+	mov bx, cx
+	xor ax, ax
+	
+@@cycm1:	
+	lodsb
+	push ax
+	loop @@cycm1
+	mov cx, bx
+	
+@@cycm2:
+	pop ax
+	stosb
+	loop @@cycm2
+	pop bp
+	ret
+	endp
 
-      mov   cx,string
-      mov   bx,010b
-      mov   dx,111b
-      
-p1:      
-      push   cx
-      and   cx,dx
-      xor   cx,bx
-      jnz   p2
-      inc    count
-p2:      
-      pop   cx
-      shl   bx,1
-      shl   dx,1
-      jnc   p1
+_word proc c uses si cx di
+	locals @@
+	lea di, delim
+	push di
+	xor al, al
+	mov cx, 65535
+	repne scasb
+	neg cx
+	push cx
+	
+@@m:	pop cx
+	pop di
+	push di
+	push cx
+	lodsb
+	repne scasb
+	jcxz @@m
+	
+	dec si
+	mov ax, si
+	add sp, 4
+	ret
+	endp
 
-	 mov ax,4c00h
-	 int 21h
-
-end      
+space proc c uses ax cx di
+	locals @@
+	lea di, delim
+	push di
+	
+	xor al, al
+	mov cx, 65535
+	repne scasb
+	neg cx
+	dec cx
+	push cx
+	
+@@m1:	pop cx
+	pop di
+	push di
+	push cx
+	lodsb
+	repne scasb
+	jcxz @@m2
+	jmp @@m1
+	
+@@m2:	dec si
+	add sp, 4 
+	ret
+	endp
+	
+	end
+	
